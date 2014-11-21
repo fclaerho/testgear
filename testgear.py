d612582eaa9bce975914ead280f19d132b65b58a
Florent Claerhout <florent.claerhout@arkena.com>
2014-11-21 11:50:07 +0100

    remove duplicated infos

--- testgear.py
+++ testgear.py
@@ -1,17 +1,6 @@
 # copyright (c) 2014 fclaerhout.fr, released under the MIT license.
 
-"""
-Test Generation Framework.
-
-Usage:
-  1. Implement the testgear.Environment interface to control the test envionment
-  2. Implement the testgear.Entity interface for all your data model entities
-  3. Call testgear.test() on the environment and entities
-  4. Enjoy
-
-Both Environment and Entity support profiles to vary the created instances.
-Testgear uses the native python unittest framework to build the testcases.
-"""
+"Test Generation Framework -- see bundled README.md for usage instructions"
 
 __version__ = "0.2"
 
