807dd8ad56b2a07ed1bb625a2e96cf46ccef36bf
florent claerhout <florent.claerhout@gmail.com>
2015-02-24 16:07:15 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import testgear
 setup(
     name = "testgear",
     version = testgear.__version__,
-    py_modules = ["testgear.py"],
+    py_modules = ["testgear"],
     test_suite = "selftest",
     description = testgear.__doc__.splitlines()[0],
     tests_require = None,
