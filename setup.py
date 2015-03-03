c98abce00b734053477f6bb17ba3c035cef76c37
florent claerhout <florent.claerhout@gmail.com>
2015-03-03 07:16:30 +0100

    add license.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 import testgear
 
 setup(
-    name = "testgear",
-    version = testgear.__version__,
-    py_modules = ["testgear"],
-    test_suite = "selftest",
-    description = testgear.__doc__.splitlines()[0],
-    tests_require = None,
-    install_requires = None)
+	name = "testgear",
+	version = testgear.__version__,
+	license = "MIT",
+	py_modules = ["testgear"],
+	test_suite = "selftest",
+	description = testgear.__doc__.splitlines()[0],
+	tests_require = None,
+	install_requires = None)
